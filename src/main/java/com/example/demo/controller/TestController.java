02c135d21e955fa7fbe5d73ec8e3c3034c6be3c1
subrat1983-git <subrat.behera.plb@gmail.com>
2026-04-18 18:00:23 +0530

    initial commit

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/welcome")
+    public String greetings() {
+        return "Hello Techie , AWS CICD Example working fine !";
+    }
+
+
+
+
+}
